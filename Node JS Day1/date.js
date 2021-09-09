a4d49cd148bfb435fe66c86ec65e58d6bec46a07
sdivyasekar <89624010+sdivyasekar@users.noreply.github.com>
2021-09-09 09:38:16 +0530

    Update date.js

--- Node JS Day1/date.js
+++ Node JS Day1/date.js
@@ -3,7 +3,7 @@ var http = require('http');
 var dt = require('./myfirstmodule');
 
 http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.writeHead(200, {'Content-Type': 'plain text'});
   res.write("The date and time is currently: " + dt.myDateTime());
   res.end();
 }).listen(8080);
