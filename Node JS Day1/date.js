a3f0e9ce8268d1a088f1aa999c804a4502d4e80d
sdivyasekar <89624010+sdivyasekar@users.noreply.github.com>
2021-10-01 13:25:31 +0530

    Delete date.js

--- Node JS Day1/date.js
+++
@@ -1,9 +0,0 @@
-
-var http = require('http');
-var dt = require('./myfirstmodule');
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'plain text'});
-  res.write("The date and time is currently: " + dt.myDateTime());
-  res.end();
-}).listen(8080);
